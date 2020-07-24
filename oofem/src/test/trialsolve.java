25edb5ab79bee1424be1d5f72686646d4d0462e0
Gursimran Singh <Gursimran Singh@DESKTOP-PREC2ID>
2020-07-24 17:56:49 +0200

    Commit Log: 1) Imported Libraries

---
+++ oofem/src/test/trialsolve.java
@@ -0,0 +1,13 @@
+package test;
+
+import iceb.jnumerics.IMatrix;
+import iceb.jnumerics.QuadraticMatrixInfo;
+import iceb.jnumerics.SolveFailedException;
+import iceb.jnumerics.lse.*;
+
+public class trialsolve {
+	public static void main (String[] args) {
+		ILSESolver solver = new BandMatrixLSESolver();
+	}
+
+}
